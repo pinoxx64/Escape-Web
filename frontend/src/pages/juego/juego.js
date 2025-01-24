46001b3797233332c81375cf66cbd8c401ecb1f5
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-01-24 11:13:11 +0100

    mostrar cosas por pantalla

--- frontend/src/pages/juego/juego.js
+++ frontend/src/pages/juego/juego.js
@@ -9,9 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
         const jugadores = await getUsersCantWithId(userId, cantJug)
         const preguntas = await getPreguntas()
 
-        //Darle a cada jugador una pregunta
-
+        console.log(preguntas)
+        //Darle al jugador una pregunta
+        const numAle = Math.floor(Math.random() * preguntas.Prueba.length)
+        const preguntaJugador = preguntas.Prueba[numAle]
+        
+        console.log(preguntaJugador)
         //Mostrar la pregunta en la pantalla, el tiempo restante, las llaves conseguidas y un boton para dar un pista (reducira el tiempo 3 minutos)
+        mostrarPregunta(preguntaJugador)
 
         //Empezar el temporizador de 30 minutos
 
@@ -25,5 +30,76 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    function mostrarPregunta(pregunta) {
+        const mainContent = document.querySelector('.main-content');
+
+        const questionElement = document.createElement('div');
+        questionElement.classList.add('question');
+        questionElement.innerHTML = `
+            <h2>Pregunta</h2>
+            <p>${pregunta.question}</p>
+        `;
+
+        const clueButtonElement = document.createElement('button');
+        clueButtonElement.classList.add('btn', 'btn-primary', 'mt-3');
+        clueButtonElement.setAttribute('data-bs-toggle', 'modal');
+        clueButtonElement.setAttribute('data-bs-target', '#clueModal');
+        clueButtonElement.textContent = 'Mostrar Pista';
+
+        const clueModalElement = document.createElement('div');
+        const clueModal = `
+                <div class="modal" id="clueModal">
+                    <div class="modal-dialog modal-md">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Pista</h4>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                    
+                            <div class="modal-body">
+                                <p>${pregunta.clue}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>       
+            `
+        const answerElement = document.createElement('div');
+        answerElement.classList.add('answer');
+        const answer = pregunta.answer;
+        console.log(answer.length)
+
+        answerElement.innerHTML = `
+        <div class="answer">
+            <label for="answer">Respuesta</label>
+            <input type="text" id="answer" class="form-control">
+            <button class="btn btn-primary mt-3" id="answerButton">Responder</button>
+        </div>
+    `
+
+        if (answer.indexOf(',') > 0) { //Select
+            let comas = 0
+            for (let i = 0; i < answer.length; i++) {
+                if (answer[i] === ',') {
+                answerElement.innerHTML += `` //Poner un select con las opciones de answerSelect 
+                }
+            }
+
+
+        }else if (answer.indexOf(':') > 0) { //Horas
+            
+        }else if (answer.indexOf('/') > 0) { //Fecha
+
+        }
+        // ver e imprimir cuantas llaves tienen
+
+        mainContent.appendChild(questionElement);
+        mainContent.appendChild(clueButtonElement);
+        mainContent.appendChild(clueModalElement);
+        mainContent.appendChild(answerElement);
+
+        document.body.insertAdjacentHTML('beforeend', clueModal);
+    }
+
+    juego()
     
 });
