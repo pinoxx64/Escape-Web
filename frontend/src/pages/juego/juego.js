10ed8ea241f60f73e1701d97b0e449f0b96b4882
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-01-26 14:57:42 +0100

    Update juego.js

--- frontend/src/pages/juego/juego.js
+++ frontend/src/pages/juego/juego.js
@@ -1,31 +1,31 @@
-import { getUsersCantWithId } from "../../components/userAPI.js"
-import { getPreguntas } from "../../components/preguntaAPI.js"
+import { getUsersCantWithId } from "../../components/userAPI.js";
+import { getPreguntas } from "../../components/preguntaAPI.js";
 
-document.addEventListener('DOMContentLoaded', function () {
-    const cantJug = sessionStorage.getItem('jugadores')
-    const userId = sessionStorage.getItem('userId')
+document.addEventListener('DOMContentLoaded', function() {
+    const cantJug = sessionStorage.getItem('jugadores');
+    const userId = sessionStorage.getItem('userId');
 
     async function juego() {
-        const jugadores = await getUsersCantWithId(userId, cantJug)
-        const preguntas = await getPreguntas()
+        const jugadores = await getUsersCantWithId(userId, cantJug);
+        const preguntas = await getPreguntas();
 
-        console.log(preguntas)
+        console.log(preguntas);
+        // Darle al jugador una pregunta
+        const preguntaJugador = preguntas.Prueba[Math.floor(Math.random() * preguntas.Prueba.length)];
 
-        const numAle = Math.floor(Math.random() * preguntas.Prueba.length)
-        const preguntaJugador = preguntas.Prueba[numAle]
+        console.log(preguntaJugador);
+        // Mostrar la pregunta en la pantalla
+        mostrarPregunta(preguntaJugador);
 
-        mostrarPregunta(preguntaJugador)
+        // Empezar el temporizador de 30 minutos
 
-        //Empezar el temporizador de 30 minutos
+        // Si el jugador responde correctamente primero, se le da una llave y se le pone el rol de almirante
 
-        //Si el jugador responde correctamente primero, se le da una llave y se le pone el rol de almirante
+        // Si el jugador es almirante mostrar un botón para que pueda dar preguntas en especifico a los demás jugadores
 
-        //Si el jugador es almirante mostrar un botón para que pueda dar preguntas en especifico a los demás jugadores
+        // Si no ha contestado primero esperar de 5 a 8 minutos y colocarle el rol de almirante a otro jugador
 
-        //Si no ha contestado primero esperar de 5 a 8 minutos y colocarle el rol de almirante a otro jugador
-
-        //Cuando termine la partida se asignaran los puntos a cada jugador: 1 por cada llave obtenida, 1 por convertirse en almirante y 2 si ganan la partida y 0 si pierden
-
+        // Cuando termine la partida se asignaran los puntos a cada jugador: 1 por cada llave obtenida, 1 por convertirse en almirante y 2 si ganan la partida y 0 si pierden
     }
 
     function mostrarPregunta(pregunta) {
@@ -38,59 +38,49 @@ document.addEventListener('DOMContentLoaded', function () {
             <p>${pregunta.question}</p>
         `;
 
-        const clueButtonElement = document.createElement('button');
-        clueButtonElement.classList.add('btn', 'btn-primary', 'mt-3');
-        clueButtonElement.setAttribute('data-bs-toggle', 'modal');
-        clueButtonElement.setAttribute('data-bs-target', '#clueModal');
-        clueButtonElement.textContent = 'Mostrar Pista';
-
-        const clueModalElement = document.createElement('div');
-        const clueModal = `
-                <div class="modal" id="clueModal">
-                    <div class="modal-dialog modal-md">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title">Pista</h4>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                            </div>
-                    
-                            <div class="modal-body">
-                                <p>${pregunta.clue}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>       
-            `
         const answerElement = document.createElement('div');
         answerElement.classList.add('answer');
         const answer = pregunta.answer;
-        console.log(answer.length)
+        console.log(answer.length);
 
-        answerElement.innerHTML = ``
+        answerElement.innerHTML = ``;
 
         if (answer.indexOf(',') > 0) {
-            let comas = 0
+            let comas = 0;
             for (let i = 0; i < answer.length; i++) {
                 if (answer[i] === ',') {
-                    comas++
+                    comas++;
                 }
             }
             if (comas > 0) {
-                answerElement.innerHTML += `
-                <select name="answerSelect" id="answerSelect" class="form-select mt-3">
-            `
-                
-            }
-            const selectElement = document.querySelector('#answerSelect');
-            if (pregunta.answerSelect != null) {
-                for (let j = 0; j < comas+1; j++) {
-                    selectElement.innerHTML += `
-                <option value="${pregunta.answerSelect[i]}">${pregunta.answerSelect[i]}</option>
-            `
+                for (let j = 0; j < comas + 1; j++) {
+                    answerElement.innerHTML += `
+                        <div class="answer">
+                            <select name="answerSelect${j}" id="answerSelect${j}" class="form-select mt-3">
+                            </select>
+                        </div>
+                    `;
                 }
             }
-            answerElement.innerHTML += `<button class="btn btn-primary mt-3" id="answerButton">Responder</button>`
 
+            const selectElements = document.querySelectorAll('.answer select');
+            console.log(pregunta.answerSelect);
+            const answerSelect = pregunta.answerSelect.split(',');
+            console.log(answerSelect);
+            if (pregunta.answerSelect != null) {
+                console.log('entra');
+                console.log(selectElements);
+                console.log(answerSelect.length);
+                for (let j = 0; j < comas+1; j++) {
+                    for (let i = 0; i < answerSelect.length; i++) {
+                        selectElements[j].innerHTML += `
+                            <option value="${answerSelect[i]}">${answerSelect[i]}</option>
+                        `;
+                        console.log(answerSelect[i]);
+                    }
+                }
+            }
+            answerElement.innerHTML += `<button class="btn btn-primary mt-3" id="answerButton">Responder</button>`;
         } else if (answer.indexOf(':') > 0) {
             answerElement.innerHTML += `
                 <div class="answer">
@@ -98,8 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <input type="time" id="answer" class="form-control">
                     <button class="btn btn-primary mt-3" id="answerButton">Responder</button>
                 </div>
-            `
-
+            `;
         } else if (answer.indexOf('/') > 0) {
             answerElement.innerHTML += `
             <div class="answer">
@@ -107,34 +96,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 <input type="date" id="answer" class="form-control">
                 <button class="btn btn-primary mt-3" id="answerButton">Responder</button>
             </div>
-        `
-        }else{
+        `;
+        } else {
             answerElement.innerHTML += `
             <div class="answer">
                 <label for="answer">Respuesta</label>
                 <input type="text" id="answer" class="form-control">
                 <button class="btn btn-primary mt-3" id="answerButton">Responder</button>
             </div>
-        `
+        `;
         }
 
-        const llavesElement = document.createElement('div');
-        llavesElement.classList.add('llaves');
-        const llaves = sessionStorage.getItem('llaves');
-        llavesElement.innerHTML = `
-            <h2>Llaves</h2>
-            <p>${llaves}/5</p>
-        `;
-
         mainContent.appendChild(questionElement);
-        mainContent.appendChild(clueButtonElement);
-        mainContent.appendChild(clueModalElement);
         mainContent.appendChild(answerElement);
-        mainContent.appendChild(llavesElement);
-
-        document.body.insertAdjacentHTML('beforeend', clueModal);
     }
 
-    juego()
-
+    juego();
 });
